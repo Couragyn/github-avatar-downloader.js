9b57dbe643bfbbc3c4f9333bf7bc95676280a836
Couragyn Chretien <couragyn@gmail.com>
2017-03-14 00:41:23 +0000

    cleaned up code, added comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,48 +1,41 @@
 var request = require('request');
 
-
-
-
 console.log('Welcome to the GitHub Avatar Downloader!');
-
-
+// create main function to get contributors
 function getRepoContributors(repoOwner, repoName, cb) {
+  // Set token variables
   var GITHUB_USER = "Couragyn";
   var GITHUB_TOKEN = "8d2ffa29ebb42473e0d745e977add3fe0db859ff";
-
+  // create request url
   var requestUrl = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`
-
+  // set options for request
   var options = {
     url: requestUrl,
     headers: {
       'User-Agent': 'request'
     }
   };
-
-
+  // create callback for results
   function callback(error, response, body) {
-      //Check for error
+      //Check  error
       if(error){
           return console.log('Error:', error);
       }
-
-      //Check for right status code
+      //Check success
       if(response.statusCode !== 200){
           return console.log('Invalid Status Code Returned:', response.statusCode);
       }
-
-      //All is good. Print the body
+      //if good, print the body
       var parsed = JSON.parse(body);
-
+      // only print if users are present
       if (parsed && parsed.length){
         for (user in parsed) {
           console.log(parsed[user].avatar_url);
         }
       }
   }
-
+  // call request
   request(options, callback);
-
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
